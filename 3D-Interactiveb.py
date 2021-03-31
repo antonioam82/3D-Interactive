fb517f5e2073be8ff2ca6260e688240820d166f1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-03-31 13:38:32 +0200

    Update 3D-Interactiveb.py

--- 3D-Interactiveb.py
+++ 3D-Interactiveb.py
@@ -6,91 +6,93 @@ from OpenGL.GLU import *
 def verts():
     global verticies
     verticies = [
-    [1, -1, -1],#det inf der
-    [1, 1, -1],#det sup der
-    [-1, 1, -1],#det sup iz
-    [-1, -1, -1],#det inf iz
-    [1, -1, 1],#del inf der
-    [1, 1, 1],#del sup der
-    [-1, -1, 1],#del inf der
-    [-1, 1, 1]#del sup iz
+    [0, 1, 0],#top
+    [1, 0, -1],#del der
+    [-1, 0, -1],#del iz
+    [1, 0, 1],#tras der
+    [-1, 0, 1],#tras iz
+    [0, -1, 0]#bottom
     ]
-
-edges = (
+    
+edges =(
     (0,1),
+    (0,2),
     (0,3),
     (0,4),
+    (1,3),
     (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
+    (4,2),
+    (4,3),
     (5,1),
-    (5,4),
-    (5,7)
+    (5,2),
+    (5,3),
+    (5,4)
     )
 
 surfaces = (
-    (0,1,2,3),
-    (3,2,7,6),
-    (6,7,5,4),
-    (4,5,1,0),
-    (1,5,7,2),
-    (4,0,3,6)
+    (3,0,4),
+    (4,0,2),
+    (2,0,1),
+    (1,0,3),
+    (3,5,4),
+    (4,5,2),
+    (2,5,1),
+    (1,5,3)
     )
 
+plane_colors = (
+    (1,2,3),
+    (2,3,4),
+    (3,4,7),
+    (2,3,4),
+    (2,3,3)
+    )
+
+
 colors = (
-    (1,0,0),
-    (0,1,0),
-    (0,0,1),
+    (1,1,0),
     (0,1,0),
     (1,1,1),
     (0,1,1),
-    (1,0,0),
+    (0,1,1),
     (0,1,0),
-    (0,0,1),
-    (0,1,0),
-    (1,1,1),
-    (0,1,1)
     )
 
-floor_verts = [
-    [20, -1, -20],
-    [-20, -1, -20],
-    [20, -1, 20],
-    [-20, -1, 20],
-    ]
-
-floor_edges = (
+plane_edges = (
     (0,1),
     (1,3),
     (3,2),
     (2,0)
     )
 
-floor_surface = (
+plane_surfaces = (
     (0,1,2,3),
     )
 
+plane_verts = [
+    [2, -1, -2],
+    [-2, -1, -2],
+    [2, -1, 2],
+    [-2, -1, 2],
+    ]
+
 def floor():
     glBegin(GL_QUAD_STRIP)
-    for surface in floor_surface:
+    for surface in plane_surfaces:
         for vertex in surface:
-            glColor3fv((0,1,1))
-            glVertex3fv(floor_verts[vertex])
+            glColor3fv((3,0,2))
+            glVertex3fv(plane_verts[vertex])
     glEnd()
-
+    
     glBegin(GL_LINES)
-    for edge in floor_edges:
+    for edge in plane_edges:
         for vertex in edge:
-            glVertex3fv(floor_verts[vertex])
+            glVertex3fv(plane_verts[vertex])
     glEnd()
-
     
 
-def Cube():
-    glBegin(GL_QUADS)
+def Triangle():
+    glBegin(GL_TRIANGLES)
     for surface in surfaces:
         x=0
         for vertex in surface:
@@ -98,7 +100,7 @@ def Cube():
             glColor3fv(colors[x])
             glVertex3fv(verticies[vertex])
     glEnd()
-    
+            
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
@@ -109,11 +111,12 @@ def Cube():
 def main():
     global verticies
     pygame.init()
-    display = (1200, 680)#(1600,900)
+    display = (1600, 900)#(1200, 680)#(1600,900)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    glClearColor(0, 0.1, 0.1, 1)
 
     verts()
-    gluPerspective(60, (display[0]/display[1]), 0.1, 50.0)
+    gluPerspective(35, (display[0]/display[1]), 0.1, 50.0)
     glTranslatef(0.0,0.0,-5)
     #glRotatef(0, 0, 0, 0)
 
@@ -122,9 +125,30 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+
         keys = pygame.key.get_pressed()
         
 
+        if keys[pygame.K_UP]:
+            glRotatef(1, -1, 0, 0)
+        if keys[pygame.K_DOWN]:
+            glRotatef(1, 1, 0, 0)
+        if keys[pygame.K_RIGHT]:
+            glRotatef(5, 0, 1, 0)
+        if keys[pygame.K_LEFT]:
+            glRotatef(5, 0, -1, 0)
+        if keys[pygame.K_k]:
+            glRotatef(1, 0, 0, 1)
+        if keys[pygame.K_l]:
+            glRotatef(1, 0, 0, -1)
+        if keys[pygame.K_p]:
+            glRotatef(0, 0, 0, 0)
+        if keys[pygame.K_c]:
+            verts()
+        if keys[pygame.K_z]:
+            glTranslatef(0.0,0.0,-0.1)
+        if keys[pygame.K_x]:
+            glTranslatef(0.0,0.0,0.1)
         if keys[pygame.K_a]:
             glTranslatef(-0.1,0.0,0.0)
         if keys[pygame.K_s]:
@@ -133,51 +157,13 @@ def main():
             glTranslatef(0.0,-0.1,0.0)
         if keys[pygame.K_w]:
             glTranslatef(0.0,0.1,0.0)
-        if keys[pygame.K_z]:
-            glTranslatef(0.0,0.0,-0.1)
-        if keys[pygame.K_x]:
-            glTranslatef(0.0,0.0,0.1)
-        if keys[pygame.K_RIGHT]:
-            verticies[2][0] += 0.09 #2
-            verticies[3][0] += 0.09
-            verticies[6][0] += 0.09
-            verticies[7][0] += 0.09
-            verticies[0][0] += 0.09 #2
-            verticies[1][0] += 0.09
-            verticies[5][0] += 0.09
-            verticies[4][0] += 0.09
-        if keys[pygame.K_LEFT]:
-            verticies[2][0] -= 0.09 #2
-            verticies[3][0] -= 0.09
-            verticies[6][0] -= 0.09
-            verticies[7][0] -= 0.09
-            verticies[0][0] -= 0.09 #2
-            verticies[1][0] -= 0.09
-            verticies[5][0] -= 0.09
-            verticies[4][0] -= 0.09
-        if keys[pygame.K_DOWN]:
-            verticies[4][2] += 0.09 
-            verticies[5][2] += 0.09
-            verticies[6][2] += 0.09
-            verticies[7][2] += 0.09
-            verticies[0][2] += 0.09 
-            verticies[1][2] += 0.09
-            verticies[2][2] += 0.09
-            verticies[3][2] += 0.09
-        if keys[pygame.K_UP]:
-            verticies[4][2] -= 0.09 
-            verticies[5][2] -= 0.09
-            verticies[6][2] -= 0.09
-            verticies[7][2] -= 0.09
-            verticies[0][2] -= 0.09 
-            verticies[1][2] -= 0.09
-            verticies[2][2] -= 0.09
-            verticies[3][2] -= 0.09            
+
         
             
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)    
+        
         floor()
-        Cube()
+        Triangle()
         
         pygame.display.flip()
         pygame.time.wait(10)
