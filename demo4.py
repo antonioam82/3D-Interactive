5e9d6b4a7e988f47482714a6300d29b9da8917c1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-06-18 22:44:16 +0200

    Update demo4.py

--- demo4.py
+++ demo4.py
@@ -1,181 +1,129 @@
-import pygame
-from pygame.locals import *
-from OpenGL.GL import *
-from OpenGL.GLU import *
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from tkinter import *
+from tkinter import Button, Label, Tk
+from tkinter import filedialog
+import cv2
+import ctypes
+import glob
+import numpy as np
+import pyautogui
+import threading
+import os
 
-verticies = [
-    [1, -1, -1],#det inf der
-    [1, 1, -1],#det sup der
-    [-1, 1, -1],#det sup iz
-    [-1, -1, -1],#det inf iz
-    [1, -1, 1],#del inf der
-    [1, 1, 1],#del sup der
-    [-1, -1, 1],#del inf der
-    [-1, 1, 1]#del sup iz
-    ]
+recording = False
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+contadores = [0,0,0]
+frame_counter = 0
 
-verticies1 = [
-    [1.5, -1.5, -1.5],#det inf der
-    [1.5, 1.5, -1.5],#det sup der
-    [-1.5, 1.5, -1.5],#det sup iz
-    [-1.5, -1.5, -1.5],#det inf iz
-    [1.5, -1.5, 1.5],#del inf der
-    [1.5, 1.5, 1.5],#del sup der
-    [-1.5, -1.5, 1.5],#del inf der
-    [-1.5, 1.5, 1.5]#del sup iz
-    ]    
+def clear_contador():
+    global contadores
+    contadores = [0,0,0]
 
-verticies2 = [
-    [2, -2, -2],#det inf der
-    [2, 2, -2],#det sup der
-    [-2, 2, -2],#det sup iz
-    [-2, -2, -2],#det inf iz
-    [2, -2, 2],#del inf der
-    [2, 2, 2],#del sup der
-    [-2, -2, 2],#del inf der
-    [-2, 2, 2]#del sup iz
-    ]
+def formato(c):
+    if c<10:
+        c="0"+str(c)
+    return c
 
-edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-    )
+def get_dir():
+    directorio_actual.set(os.getcwd())
+
+def screen_s():
+    user32 = ctypes.windll.user32
+    user32.SetProcessDPIAware()
+    dimensions = user32.GetSystemMetrics(0),user32.GetSystemMetrics(1)
+    return dimensions
+
+def file_name(tex,ext):
+    count = 0
+    for i in glob.glob('*'+ext):
+        if tex in i:
+            count+=1
+    if count>0:
+        filename=tex+" "+str(count)+ext
+    else:
+        filename=tex+ext
+    return filename
+
+def screen_shoot():
+    pyautogui.screenshot(file_name("screenshoot",".jpg"))
+
+def cuenta(n):
+    global contadores,frame_counter
+    clock['text'] = str(contadores[0])+":"+str(formato(contadores[1]))+":"+str(formato(contadores[2]))
+    if n == 20.0:
+        contadores[2]+=1
+        frame_counter = 0
         
-
-def increase(o):
-    for i in lista_cubis:
-        if o == "inc":
-            i[0][0]+=0.05
-            i[0][1]-=0.05
-            i[0][2]-=0.05
-            i[1][0]+=0.05
-            i[1][1]+=0.05
-            i[1][2]-=0.05
-            i[2][0]-=0.05
-            i[2][1]+=0.05
-            i[2][2]-=0.05
-            i[3][0]-=0.05
-            i[3][1]-=0.05
-            i[3][2]-=0.05
-            i[4][0]+=0.05
-            i[4][1]-=0.05
-            i[4][2]+=0.05
-            i[5][0]+=0.05
-            i[5][1]+=0.05
-            i[5][2]+=0.05
-            i[6][0]-=0.05
-            i[6][1]-=0.05
-            i[6][2]+=0.05
-            i[7][0]-=0.05
-            i[7][1]+=0.05
-            i[7][2]+=0.05
-        else:
-            i[0][0]-=0.05
-            i[0][1]+=0.05
-            i[0][2]+=0.05
-            i[1][0]-=0.05
-            i[1][1]-=0.05
-            i[1][2]+=0.05
-            i[2][0]+=0.05
-            i[2][1]-=0.05
-            i[2][2]+=0.05
-            i[3][0]+=0.05
-            i[3][1]+=0.05
-            i[3][2]+=0.05
-            i[4][0]-=0.05
-            i[4][1]+=0.05
-            i[4][2]-=0.05
-            i[5][0]-=0.05
-            i[5][1]-=0.05
-            i[5][2]-=0.05
-            i[6][0]+=0.05
-            i[6][1]+=0.05
-            i[6][2]-=0.05
-            i[7][0]+=0.05
-            i[7][1]-=0.05
-            i[7][2]-=0.05
-            
-            
-lista_cubis = [verticies,verticies1,verticies2]
-colores_linea = [(1,0,0),(0,0,1),(0,1,0)]
-print(colores_linea[0][0])
-def Cube():
-    glBegin(GL_LINES)
-    #glColor3d(1, 0, 0)
-    c=0
-    for i in lista_cubis:
-        glColor3d(colores_linea[c][0],colores_linea[c][1],colores_linea[c][2])
-        for edge in edges:
-            for vertex in edge:
-                glVertex3fv(i[vertex])
-        c+=1
-
-    glEnd()
+    if contadores[2]==60:
+        contadores[2]=0
+        contadores[1]+=1
+    if contadores[1]==60:
+        contadores[1]=0
+        contadores[0]+=1
     
-def main():
-    #global verticies
-    pygame.init()
-    display = (1200, 680)#(1600,900)
-    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-    glTranslatef(0.0,0.0,-5)
-    #glRotatef(0, 0, 0, 0)
+def record_state():
+    global out
+    global recording
+    if recording == True:
+        recording = False
+    else:
+        clear_contador()
+        recording = True
+        recorder.configure(text="Stop")
+        t1=threading.Thread(target=record)
+        t1.start()
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-        keys = pygame.key.get_pressed()
         
+def direct():
+    directorio=filedialog.askdirectory()
+    if directorio!="":
+        os.chdir(directorio)
+        directorio_actual.set(os.getcwd())
 
-        if keys[pygame.K_UP]:
-            glRotatef(1, -1, 0, 0)
-        if keys[pygame.K_DOWN]:
-            glRotatef(1, 1, 0, 0)
-        if keys[pygame.K_RIGHT]:
-            glRotatef(1, 0, 1, 0)
-        #if keys[pygame.K_LEFT]:
-            #glRotatef(1, 0, -1, 0)
-        if keys[pygame.K_k]:
-            glRotatef(1, 0, 0, 1)
-        if keys[pygame.K_l]:
-            glRotatef(1, 0, 0, -1)
-        if keys[pygame.K_c]:
-            verts()
-        if keys[pygame.K_z]:
-            glTranslatef(0.0,0.0,-0.25)#-0.1)
-        if keys[pygame.K_x]:
-            glTranslatef(0.0,0.0,0.25)#0.1)
-        if keys[pygame.K_i]:
-            increase("inc")
-        if keys[pygame.K_o]:
-            increase("dec")
-        if keys[pygame.K_a]:
-            glTranslatef(-0.25,0.0,0.0)#(-0.1,0.0,0.0)
-
-        if keys[pygame.K_s]:
-            glTranslatef(0.25,0.0,0.0)#(0.1,0.0,0.0)
-        if keys[pygame.K_q]:
-            glTranslatef(0.0,-0.1,0.0)
-        if keys[pygame.K_w]:
-            glTranslatef(0.0,0.1,0.0)
-            
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)    
-        Cube()
-        glRotatef(1, 0, -1, 0)
+def record():
+    global out, frame_counter, fail
+    fail = False
+    frameRate = 20
+    out = cv2.VideoWriter(file_name("screenvideo",".mp4"), fourcc, frameRate, (screen_size))#20.0 18.2 #17
+    while recording == True:
+        try:
+            img = pyautogui.screenshot()
+            frame = np.array(img)
+            frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+            out.write(frame)
+            frame_counter+=1
+            cuenta(frame_counter)
+        except Exception as e:
+            #print(str(e))
+            fail = True
+            break
         
-        pygame.display.flip()
-        pygame.time.wait(10)
+    if fail == False:
+        print(frame_counter)
+        recorder.configure(text="Record")
+        out.release()
 
-main()
+ventana = Tk()
+ventana.title("Screen Recorder")
+ventana.geometry("507x143")
+ventana.configure(bg="light gray")
+directorio_actual=StringVar()
+screen_size = screen_s()
+
+
+Dirlabel = Entry(ventana,bg="white",width=90,textvariable=directorio_actual)
+Dirlabel.pack(padx=1,pady=1)
+clock = Label(ventana, fg='green', width=21, text="0:00:00", bg="black", font=("","29"))#text="00:00:00"
+clock.pack(pady=10)
+recorder = Button(ventana,text="Record",bg="light blue",fg="red",width=33,command=record_state)#gray66
+recorder.place(x=11,y=88)
+shoot = Button(ventana,text="Screenshot",bg="light blue",fg="red",width=33,command=screen_shoot)
+shoot.place(x=255,y=88)
+
+folder = Button(ventana,text="Select Folder",bg="gray66",width=68,command=direct)
+folder.pack(padx=1,side='bottom')
+
+get_dir()
+
+ventana.mainloop()
